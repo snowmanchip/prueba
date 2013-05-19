bc8b063aa71a2c65ccd44b4fa7b63ad7ab1a1d3a
Andrés Chiponti <snowmanchip@gmail.com>
2013-05-19 19:21:37 -0400

    Cargamos funciones de autentificación PHP
    Funciones Versión 2010

---
+++ data_valid_fns.php
@@ -0,0 +1,22 @@
+<?php
+//Revisa si cada variable tiene un valor
+function filled_out($form_vars) {
+	
+	foreach ($form_vars as $key => $value) {
+		if (!isset($key) || ($value =='')) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Revisamos si el correo tiene una dirección válida
+function valid_email($adress){
+	if (filter_var($adress, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $adress)){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+?>
